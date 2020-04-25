cabe4fdb31f53ebf0405d266045b34c7d179c235
hiroya8649 <happy8649@gmail.com>
2020-04-25 16:17:07 +0800

    27

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from "graphql-yoga";
+import uuidv4 from "uuid/v4";
 
 // Scalar types - String, Boolean, Int, Float, ID
 
@@ -35,79 +36,103 @@ const posts = [
     title: "GraphQL 201",
     body: "This is an advanced GraphQL post...",
     published: false,
-    author: "2",
+    author: "1",
   },
   {
     id: "12",
     title: "Programming Music",
     body: "",
-    published: false,
-    author: "3",
+    published: true,
+    author: "2",
   },
 ];
 
 const comments = [
   {
-    id: "21",
-    text: "Nice",
+    id: "102",
+    text: "This worked well for me. Thanks!",
+    author: "3",
+    post: "10",
+  },
+  {
+    id: "103",
+    text: "Glad you enjoyed it.",
     author: "1",
     post: "10",
   },
   {
-    id: "22",
-    text: "To",
-    author: "1",
+    id: "104",
+    text: "This did no work.",
+    author: "2",
     post: "11",
   },
   {
-    id: "23",
-    text: "Show",
-    author: "2",
-    post: "12",
-  },
-  ,
-  {
-    id: "24",
-    text: "You",
-    author: "3",
-    post: "12",
+    id: "105",
+    text: "Nevermind. I got it to work.",
+    author: "1",
+    post: "11",
   },
 ];
 
 // Type definitions (schema)
 const typeDefs = `
-  type Query {
-    users(query: String): [User!]!
-    posts(query: String): [Post!]!
-    comments: [Comment!]!
-    me: User!
-    post: Post!
-  }
+    type Query {
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        comments: [Comment!]!
+        me: User!
+        post: Post!
+    }
 
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    age: Int
-    posts: [Post!]!
-    comments: [Comment!]!
-  }
+    type Mutation {
+        createUser(data: CreateUserInput!): User!
+        createPost(data: CreatePostInput!): Post!
+        createComment(data: CreateCommentInput!): Comment!
+    }
 
-  type Post {
-    id: ID!
-    title: String!
-    body: String!
-    published: Boolean!
-    author: User!
-    comments: [Comment!]!
-  }
+    input CreateUserInput {
+        name: String!
+        email: String!
+        age: Int
+    }
 
-  type Comment {
-    id: ID!
-    text: String!
-    author: User!
-    post: Post!
-  }
+    input CreatePostInput {
+        title: String!
+        body: String!
+        published: Boolean!
+        author: ID!
+    }
+
+    input CreateCommentInput {
+        text: String!
+        author: ID!
+        post: ID!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+        posts: [Post!]!
+        comments: [Comment!]!
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
+        comments: [Comment!]!
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
+    }
 `;
 
 // Resolvers
@@ -137,7 +162,7 @@ const resolvers = {
         return isTitleMatch || isBodyMatch;
       });
     },
-    comments() {
+    comments(parent, args, ctx, info) {
       return comments;
     },
     me() {
@@ -156,6 +181,59 @@ const resolvers = {
       };
     },
   },
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      const emailTaken = users.some((user) => user.email === args.data.email);
+
+      if (emailTaken) {
+        throw new Error("Email taken");
+      }
+
+      const user = {
+        id: uuidv4(),
+        ...args.data,
+      };
+
+      users.push(user);
+
+      return user;
+    },
+    createPost(parent, args, ctx, info) {
+      const userExists = users.some((user) => user.id === args.data.author);
+
+      if (!userExists) {
+        throw new Error("User not found");
+      }
+
+      const post = {
+        id: uuidv4(),
+        ...args.data,
+      };
+
+      posts.push(post);
+
+      return post;
+    },
+    createComment(parent, args, ctx, info) {
+      const userExists = users.some((user) => user.id === args.data.author);
+      const postExists = posts.some(
+        (post) => post.id === args.data.post && post.published
+      );
+
+      if (!userExists || !postExists) {
+        throw new Error("Unable to find user and post");
+      }
+
+      const comment = {
+        id: uuidv4(),
+        ...args.data,
+      };
+
+      comments.push(comment);
+
+      return comment;
+    },
+  },
   Post: {
     author(parent, args, ctx, info) {
       return users.find((user) => {
@@ -168,18 +246,6 @@ const resolvers = {
       });
     },
   },
-  User: {
-    posts(parent, args, ctx, info) {
-      return posts.filter((post) => {
-        return post.author === parent.id;
-      });
-    },
-    comments(parent, args, ctx, info) {
-      return comments.filter((comment) => {
-        return comment.author === parent.id;
-      });
-    },
-  },
   Comment: {
     author(parent, args, ctx, info) {
       return users.find((user) => {
@@ -189,6 +255,18 @@ const resolvers = {
     post(parent, args, ctx, info) {
       return posts.find((post) => {
         return post.id === parent.post;
+      });
+    },
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => {
+        return post.author === parent.id;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.author === parent.id;
       });
     },
   },
