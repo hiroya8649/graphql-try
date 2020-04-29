ae46e4e9fa0c362803172e59fffc94cb4b3ee067
hiroya8649 <happy8649@gmail.com>
2020-04-29 15:25:41 +0800

    37

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -23,6 +23,11 @@ const Subscription = {
       return pubsub.asyncIterator(`comment ${postId}`);
     },
   },
+  post: {
+    subscribe(parent, args, { db, pubsub }, info) {
+      return pubsub.asyncIterator(`post`);
+    },
+  },
 };
 
 export { Subscription as default };
